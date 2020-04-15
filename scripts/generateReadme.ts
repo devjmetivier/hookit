2de220ec27300a985c0641d148b3732d9efec293
Devin Metivier <devinmetivier@gmail.com>
2020-04-14 21:28:05 -0400

    chore - coverge badge

--- scripts/generateReadme.ts
+++ scripts/generateReadme.ts
@@ -11,6 +11,9 @@ const template = `<h1 align="center">🪝 Hooky 🪝</h1>
   <a aria-label="License" href="https://github.com/devjmetivier/hooky/pulls?q=is%3Apr+is%3Aopen+">
     <img src="https://img.shields.io/github/issues-pr-raw/devjmetivier/hooky" />
   </a>
+  <a aria-label="Codecov" href="https://codecov.io/gh/devjmetivier/hooky">
+    <img src="https://codecov.io/gh/devjmetivier/hooky/branch/master/graph/badge.svg" />
+  </a>
   <a aria-label="License" href="https://github.com/devjmetivier/hooky/blob/master/LICENSE">
     <img src="https://img.shields.io/github/license/devjmetivier/hooky" />
   </a>
