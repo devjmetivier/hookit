3808aaeb2346ee6611f2fff4d50962a1f72f2089
Devin Metivier <devin.metivier@lcecorp.com>
2020-04-23 16:49:05 -0400

    chore - dark mode code

--- packages/dark-mode/src/useDarkMode.ts
+++ packages/dark-mode/src/useDarkMode.ts
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import usePrefersDarkMode from '@hooky/prefers-dark-mode';
+import useLocalStorage from '@hooky/local-storage';
 
-const useDarkMode = () => {
-  const [state, setState] = React.useState();
+const useDarkMode = (className: string) => {
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode');
+  const prefersDarkMode = usePrefersDarkMode();
 
-  return [state, setState];
+  const enabled = typeof darkMode !== 'undefined' ? darkMode : prefersDarkMode;
+
+  React.useEffect(() => {
+    const { body } = window.document;
+
+    if (enabled) {
+      body.classList.add(className);
+    } else {
+      body.classList.remove(className);
+    }
+  }, [enabled, className]);
+
+  return [darkMode, setDarkMode];
 };
 
 export default useDarkMode;
