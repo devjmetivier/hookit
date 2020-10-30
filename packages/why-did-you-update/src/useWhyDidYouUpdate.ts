e271dacc4f0156b70161c59c1b86d6047bc2e088
Devin Metivier <devinmetivier@gmail.com>
2020-10-30 00:00:01 -0400

    feature - add useWhyDidYouUpdate

---
+++ packages/why-did-you-update/src/useWhyDidYouUpdate.ts
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+export type Name = string;
+export type Props = { [key: string]: any };
+
+const useWhyDidYouUpdate = (name: Name, props: { [key: string]: any }) => {
+  const prevProps = React.useRef<typeof props>();
+
+  React.useEffect(() => {
+    if (prevProps.current) {
+      const allKeys = Object.keys({ ...prevProps.current, ...props });
+
+      const changesObj: typeof props = {};
+
+      allKeys.forEach((key) => {
+        if (prevProps.current[key] !== props[key]) {
+          changesObj[key] = {
+            from: prevProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+
+      if (Object.keys(changesObj).length) {
+        console.log('[why-did-you-update]:', `${name}\n`, changesObj);
+      }
+    }
+
+    prevProps.current = props;
+  }, [name, props]);
+};
+
+export default useWhyDidYouUpdate;
