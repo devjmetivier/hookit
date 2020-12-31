dbdc42f1f93b1e559a0ab854759694564ef8894c
Devin Metivier <devinmetivier@gmail.com>
2020-12-31 00:26:38 -0500

    feature - usePermissions initial commit

---
+++ packages/permissions/src/server.test.ts
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment node
+ */
+
+import renderHookServer from '../../../utils/renderHookServer';
+
+import usePermissions from './usePermissions';
+
+describe('usePermissions SSR', () => {
+  it('does this', () => {
+    // const renderedHook = renderHookServer(() => usePermissions());
+
+    expect(true).toBeTruthy();
+  });
+});
