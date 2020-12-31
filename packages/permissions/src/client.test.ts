873a824fcc1c8aa36cea98dca426c4131cb9b457
Devin Metivier <devinmetivier@gmail.com>
2020-12-31 00:26:38 -0500

    feature - usePermissions initial commit

---
+++ packages/permissions/src/client.test.ts
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import usePermissions from './usePermissions';
+
+describe('usePermissions CSR', () => {
+  it('does this', () => {
+    // const renderedHook = renderHook(() => usePermissions());
+
+    expect(true).toBeTruthy();
+  });
+});
