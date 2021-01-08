36cb6278d2e548a17ea56ce0867d07239b485683
Devin Metivier <devin.metivier@lcecorp.com>
2021-01-08 15:14:12 -0500

    feature - fix errors, add tests, add docs

--- packages/permissions/src/usePermissions.tsx
+++ packages/permissions/src/usePermissions.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { forgotArgs, mustBeBoolean, usePermissionsErrorLabel } from './messages';
 
 export type TPermissions<P extends string = string> = Partial<
   { [K in P]: boolean | ((args: any) => boolean) | ((args: any) => Promise<boolean>) }
@@ -42,27 +43,23 @@ export const usePermissions = <Permissions extends string = string, Roles extend
         let result = (currentRole[permission] as Function)(permissionData);
 
         if (typeof result !== 'boolean') {
-          console.error('Permissions in rules must be of type `boolean` or a function that returns a boolean');
+          console.error(mustBeBoolean);
           return false;
         }
 
         return result;
       } catch (error) {
-        console.log('%cusePermissions error:', 'color: red; font-size: 20px');
+        console.log(...usePermissionsErrorLabel);
         console.error({ error });
 
-        if (process.env.NODE_ENV === 'development') {
-          throw new Error(
-            'This error likely occurred because you forgot to pass the proper arguments to the `canAccess` method of the `usePermissions` hook. Check your console for a more detailed error. In production builds, this error will only occur in the console and return false as a default.',
-          );
-        }
+        forgotArgs();
 
         return false;
       }
     }
 
     if (typeof currentRole[permission] !== 'boolean') {
-      console.error('Permissions in rules must be of type `boolean` or a function that returns a boolean');
+      console.error(mustBeBoolean);
       return false;
     }
 
