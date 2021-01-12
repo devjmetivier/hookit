3545fe552ee02e3f3c6d1e276eb3a8dea64c9641
Devin Metivier <devinmetivier@gmail.com>
2021-01-12 15:31:07 +0000

    feature - update template

--- scripts/templates/use-.ts.ts
+++ scripts/templates/use-.ts.ts
@@ -2,11 +2,9 @@ import { ICliArgs } from '../newPackage';
 
 export default ({ hookName }: ICliArgs) => `import * as React from 'react';
 
-const ${hookName} = () => {
+export const ${hookName} = () => {
   const [state, setState] = React.useState();
 
   return [state, setState];
 };
-
-export default ${hookName};
 `;
