2dc8055b53af2fee9136a3a681648a7592711bb6
Devin Metivier <devinmetivier@gmail.com>
2023-03-27 16:46:37 -0400

    updates

--- scripts/templates/tsconfig.json.ts
+++ scripts/templates/tsconfig.json.ts
@@ -1,9 +1,10 @@
 export default () => `{
   "compilerOptions": {
-    "outDir": "./dist"
+    "outDir": "./dist",
+    "moduleResolution": "node"
   },
   "extends": "tsconfig/base.json",
-  "include": ["src/**/*.ts"],
+  "include": ["src/**/*.ts", "src/**/*.tsx"],
 "exclude": ["node_modules", "**/*.test.ts", "**/*.test.tsx"]
 }
 `;
